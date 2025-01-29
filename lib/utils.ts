749bac6603a480f690769f4b626c001c10328d31
PhanTrungDuc <phantrungduc2522005@gmail.com>
2025-01-30 00:00:37 +0700

    setup plaid + dwolla cho chức năng banking

--- lib/utils.ts
+++ lib/utils.ts
@@ -207,5 +207,5 @@ export const authFormSchema = (type: string) => z.object({
     state: type === "sign-in" ? z.string().optional() : z.string().min(2),
     postalCode: type === "sign-in" ? z.string().optional() : z.string().min(3).max(6),
     dateOfBirth: type === "sign-in" ? z.string().optional() : z.string().min(3),
-    snn: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    ssn: type === "sign-in" ? z.string().optional() : z.string().min(3),
 })
