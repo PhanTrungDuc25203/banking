5c29c0323817798651b23d695c4134bcab04641f
PhanTrungDuc <phantrungduc2522005@gmail.com>
2025-01-29 15:49:59 +0700

    cài đặt sentry để theo dõi hành động người dùng

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://2eb4b924a55f410220b01e1e7ceb072a@o4508726014902272.ingest.us.sentry.io/4508726017392640",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
